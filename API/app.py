6b889a9ef0e7a375281fb228823e8cfd969a2402
Victor Vaca <victorhvr.net@gmail.com>
2026-02-25 15:23:30 -0500

    método de platzi agregado

--- API/app.py
+++ API/app.py
@@ -5,12 +5,26 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+
 @app.get("/familia")
 def get_familia():
     rows = ["Amin", "Marce", "Miranda"]
     return rows
 
+
 @app.get("/superheroesDC")
 def get_superheroes():
     rows = ["Superman", "Batman", "Flash", "Linterna Verde", "Mujer maravilla", "Aquaman", "Shazam", "Cyborg"]
     return rows
+
+
+@app.get("/superheroesMarvel")
+def get_superheroes_marvel():
+    rows = ["Spider-Man", "Iron Man", "Thor", "Hulk", "Black Widow", "Doctor Strange", "Black Panther", "Captain America"]
+    return rows
+
+
+@app.get("/cursosPlatzi")
+def get_cursos():
+    cursos = ["Docker", "Bash", "Linux", "Inglés","Python" , "Javascript", "Azure", "DevOps"]
+    return cursos
